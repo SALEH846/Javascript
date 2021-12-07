126993973ab9c7002a97e3237c48a13d622b0069
SALEH846 <ranaasaleh846@gmail.com>
2021-12-07 23:36:22 +0500

    const keyword

---
+++ Modern JS (ES6)/const.js
@@ -0,0 +1,6 @@
+// const keyword
+// Variables defined with const keyword are also block scoped.
+// Once the variable is assigned with a value and defined with const keyword. It can not be reassigned.
+
+const x = 2;
+// x = 1; // throw an error
