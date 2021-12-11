8fe6a4d02cf4ee2f5ff526ea9000e5e0e014dad5
SALEH846 <ranaasaleh846@gmail.com>
2021-12-11 20:05:15 +0500

    'this' keyword in JS

---
+++ Modern JS (ES6)/this_keyword.js
@@ -0,0 +1,19 @@
+const person = {
+    name: "Saleh",
+    walk() {
+        console.log(this);
+    }
+};
+
+person.walk(); // will return the reference to the object in which it is defined
+
+console.log("-------------------");
+const walk = person.walk;
+walk(); // here, it will return the global object i.e. window object because the function walk is now defined
+// in global scope
+// Solution to this problem is
+console.log("--------------------");
+// Funstions in JS are objects so they also have methods such as bind
+const bindedWalk = person.walk.bind(person); // we are passing the person object in place of this so it will bind
+// with the person object
+bindedWalk();
