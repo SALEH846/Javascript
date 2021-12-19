84ed8b81bdabadf3e5fc5775bfa29de8ab6fcbbe
SALEH846 <ranaasaleh846@gmail.com>
2021-12-19 21:39:43 +0500

    Example of Synchronous

---
+++ Asynchronous JavaScript/synchronous.js
@@ -0,0 +1,9 @@
+// Synchronous
+// The following lines will print one by one in sequence 
+
+console.log(" I ");
+console.log(" will ");
+console.log(" go ");
+console.log(" to ");
+console.log(" university ");
+console.log(" today ");
