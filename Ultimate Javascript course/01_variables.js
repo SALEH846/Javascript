d430e51cfa52367b8d156147441e13256664b9c5
SALEH846 <ranaasaleh846@gmail.com>
2022-07-22 16:02:22 +0500

    Variables in JavaScript

---
+++ Ultimate Javascript course/01_variables.js
@@ -0,0 +1,25 @@
+var test = 67 // a contains 67
+console.log(test)
+
+test = "harry" // we can change data type of a variable in a program --> JS is dynamically typed programming language
+console.log(test)
+
+// let 8harry = 7 // Not allowed this will throw an error --> invalid varibale name --> cannot start with a number
+// let var = 7 // invalid varibale name --> keyword `var` used as variable name
+
+let _ = "saleh" // valid varibale name
+console.log(_)
+
+let _xyz = "haseeb" // valid varibale name
+console.log(_xyz)
+
+let $abc = 7.67 // valid varibale name
+console.log($abc)
+
+// valid varibale names can only be started with `_`, `$` and any letter and nothing else.
+// cannot be started with any number or any special character other than `_` and `$`
+
+// JavaScript is a case sensitive programming language.
+
+// my_name --> snake case
+// myName --> camel case
